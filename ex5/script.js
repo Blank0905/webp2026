fbe3235fdcfa2a17972d5794773831c79fcb48a5
Blank <zhizhang808@gmail.com>
2026-04-08 14:43:58 +0800

    實作EX5

---
+++ ex5/script.js
@@ -0,0 +1,21 @@
+var count = 1;
+
+function addfunction() {
+    var btn = document.createElement("BUTTON");
+    btn.innerHTML = `CLICK ME (${count})`;
+    btn.setAttribute("id", "btn" + count);
+    btn.setAttribute("class", "btn btn-outline-danger");
+    count++;
+    document.body.appendChild(btn);
+};
+
+function delfunction(){
+    if(count > 1) {
+        var btn = document.getElementById("btn" + --count);
+        document.body.removeChild(btn);
+    }
+    else{
+        alert("要變FOOD了")
+    }
+    
+};
